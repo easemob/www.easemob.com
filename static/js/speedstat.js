e9f458afa655b463d6f797ba7d261ec10071f4e1
Liu Lantao <liulantao@gmail.com>
2014-11-11 17:32:08 +0800

    speedstat: add sjhl-beijing

--- static/js/speedstat.js
+++ static/js/speedstat.js
@@ -4,20 +4,22 @@ try{
 		var report_url = "http://s.easemob.com/s/net";
 
 		var tasks = {
-			1 : ["qq-guangzhou-1k", "http://203.195.202.39/s/1k.html"],
-			2 : ["qq-guangzhou-10k", "http://203.195.202.39/s/10k.html"],
-			3 : ["ali-beijing-1k", "http://182.92.159.193/1k.html"],
-			4 : ["ali-beijing-10k", "http://182.92.159.193/10k.html"],
-			5 : ["qing-guangzhou-1k", "http://121.201.15.126/1k.html"],
-			6 : ["qing-guangzhou-10k", "http://121.201.15.126/10k.html"],
-			7 : ["qing-beijing2-1k", "http://119.254.108.53/1k.html"],
-			8 : ["qing-beijing2-10k", "http://119.254.108.53/10k.html"],
-			9 : ["aws-cn-1k", "http://54.223.147.200:8090/1k.html"],
-			10 : ["aws-cn-10k", "http://54.223.147.200:8090/10k.html"],
-			11 : ["ali-hangzhou-1k", "http://121.41.37.18/1k.html"],
-			12 : ["ali-hangzhou-10k", "http://121.41.37.18/10k.html"],
-			13 : ["ucloud-beijing-1k", "http://180.150.186.246/1k.html"],
-			14 : ["ucloud-beijing-10k", "http://180.150.186.246/10k.html"]
+			1 : ["aws-cn-1k", "http://54.223.147.200:8090/1k.html"],
+			3 : ["ali-hangzhou-1k", "http://121.41.37.18/1k.html"],
+			5 : ["ucloud-beijing-1k", "http://180.150.186.246/1k.html"],
+			7 : ["qq-guangzhou-1k", "http://203.195.202.39/s/1k.html"],
+			9 : ["ali-beijing-1k", "http://182.92.159.193/1k.html"],
+			11 : ["qing-guangzhou-1k", "http://121.201.15.126/1k.html"],
+			13 : ["qing-beijing2-1k", "http://119.254.108.53/1k.html"],
+			15 : ["sjhl-beijing-1k", "http://59.151.45.42/1k.html"],
+			2 : ["aws-cn-10k", "http://54.223.147.200:8090/10k.html"],
+			4 : ["ali-hangzhou-10k", "http://121.41.37.18/10k.html"],
+			6 : ["ucloud-beijing-10k", "http://180.150.186.246/10k.html"],
+			8 : ["qq-guangzhou-10k", "http://203.195.202.39/s/10k.html"],
+			10 : ["ali-beijing-10k", "http://182.92.159.193/10k.html"],
+			12 : ["qing-guangzhou-10k", "http://121.201.15.126/10k.html"],
+			14 : ["qing-beijing2-10k", "http://119.254.108.53/10k.html"],
+			16 : ["sjhl-beijing-1k", "http://59.151.45.42/10k.html"]
 		};
 		var _count = 14; // length of tasks[]
 		var _submited = false;
