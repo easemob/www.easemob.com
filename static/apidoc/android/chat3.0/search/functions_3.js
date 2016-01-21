d5fb9d627e6ba5209f520926335230287d2962b1
zhanw <258653667@qq.com>
2016-01-21 19:34:08 +0800

    add 3.0 doc

---
+++ static/apidoc/android/chat3.0/search/functions_3.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['declineapplication',['declineApplication',['../classcom_1_1hyphenate_1_1chat_1_1_e_m_group_manager.html#a9bc0c85ab2dabd4352d1f1f65d6e2427',1,'com::hyphenate::chat::EMGroupManager']]],
+  ['declineinvitation',['declineInvitation',['../classcom_1_1hyphenate_1_1chat_1_1_e_m_contact_manager.html#a9ad0958d5b9de677cce7eaef4e0337fd',1,'com.hyphenate.chat.EMContactManager.declineInvitation()'],['../classcom_1_1hyphenate_1_1chat_1_1_e_m_group_manager.html#ae71c2cd6a0cd5f6a0d069f764ce958c1',1,'com.hyphenate.chat.EMGroupManager.declineInvitation()']]],
+  ['decrypt',['decrypt',['../interfacecom_1_1hyphenate_1_1chat_1_1_encrypt_provider.html#ad18bbab84fa26d656e8a672b94c3ab68',1,'com::hyphenate::chat::EncryptProvider']]],
+  ['decryptfile',['decryptFile',['../classcom_1_1hyphenate_1_1chat_1_1_e_m_encrypt_utils.html#a5aaf1ba5fe1cf85f367be46066c3c453',1,'com::hyphenate::chat::EMEncryptUtils']]],
+  ['deletecontact',['deleteContact',['../classcom_1_1hyphenate_1_1chat_1_1_e_m_contact_manager.html#afc04729eb6d43609d05521c19c5450e0',1,'com::hyphenate::chat::EMContactManager']]],
+  ['deleteconversation',['deleteConversation',['../classcom_1_1hyphenate_1_1chat_1_1_e_m_chat_manager.html#ae00d3eb41e070546b6b7dc1c3e0750c3',1,'com::hyphenate::chat::EMChatManager']]],
+  ['destroygroup',['destroyGroup',['../classcom_1_1hyphenate_1_1chat_1_1_e_m_group_manager.html#a860a34ecd342b1e15d3e6be7e31879be',1,'com::hyphenate::chat::EMGroupManager']]],
+  ['direct',['direct',['../classcom_1_1hyphenate_1_1chat_1_1_e_m_message.html#a63a9692bdcc9c7da476e42141c338c84',1,'com::hyphenate::chat::EMMessage']]],
+  ['downloadattachment',['downloadAttachment',['../classcom_1_1hyphenate_1_1chat_1_1_e_m_chat_manager.html#a1a938a579e8e5e07518d1e5008f9241d',1,'com::hyphenate::chat::EMChatManager']]],
+  ['downloadfile',['downloadFile',['../classcom_1_1hyphenate_1_1chat_1_1_e_m_chat_manager.html#af39b20828a22e8a13ace18a8335ac50a',1,'com::hyphenate::chat::EMChatManager']]],
+  ['downloadthumbnail',['downloadThumbnail',['../classcom_1_1hyphenate_1_1chat_1_1_e_m_chat_manager.html#a4c4f426c7cb00d44fee5d550a7eea5ce',1,'com::hyphenate::chat::EMChatManager']]]
+];
---
+++ static/apidoc/android/chat3.0/search/searchdata.js
@@ -0,0 +1,30 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefghijlmoprstuå",
+  1: "cdelmst",
+  2: "c",
+  3: "abcdefghijlmoprstu",
+  4: "acdegiprsu",
+  5: "å"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "functions",
+  4: "variables",
+  5: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "全部",
+  1: "类",
+  2: "命名空间",
+  3: "函数",
+  4: "变量",
+  5: "页"
+};
+
