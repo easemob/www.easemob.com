3a07cb5577a15484b571a23d7105caded15c15b5
linan <linan@easemob.com>
2017-11-30 11:52:42 +0800

    android 3.3.7 doc

--- static/apidoc/android/chat3.0/search/classes_5.js
+++ static/apidoc/android/chat3.0/search/classes_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['status',['Status',['../enumcom_1_1hyphenate_1_1chat_1_1_e_m_call_session_1_1_status.html',1,'com.hyphenate.chat.EMCallSession.Status'],['../enumcom_1_1hyphenate_1_1chat_1_1_e_m_message_1_1_status.html',1,'com.hyphenate.chat.EMMessage.Status']]]
+  ['status',['Status',['../enumcom_1_1hyphenate_1_1chat_1_1_e_m_call_session_1_1_status.html',1,'com::hyphenate::chat::EMCallSession']]],
+  ['status',['Status',['../enumcom_1_1hyphenate_1_1chat_1_1_e_m_message_1_1_status.html',1,'com::hyphenate::chat::EMMessage']]]
 ];
