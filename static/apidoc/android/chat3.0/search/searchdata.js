9c4351dc0527338eac236e4dbfdf97a9a90dc300
Zhang Song <862931923@qq.com>
2018-08-13 15:41:52 +0800

    更新api文档至3.5.0

--- static/apidoc/android/chat3.0/search/searchdata.js
+++ static/apidoc/android/chat3.0/search/searchdata.js
@@ -2,32 +2,29 @@ var indexSectionsWithContent =
 {
   0: "abcdefgijklmnoprstuvå",
   1: "cdelmst",
-  2: "c",
-  3: "abcdefgijklmoprstu",
-  4: "acdefgimnrsuv",
-  5: "v",
-  6: "å"
+  2: "abcdefgijklmoprstu",
+  3: "acdefgimnrsuv",
+  4: "v",
+  5: "å"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "namespaces",
-  3: "functions",
-  4: "variables",
-  5: "enumvalues",
-  6: "pages"
+  2: "functions",
+  3: "variables",
+  4: "enumvalues",
+  5: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "全部",
   1: "类",
-  2: "命名空间",
-  3: "函数",
-  4: "变量",
-  5: "枚举值",
-  6: "页"
+  2: "函数",
+  3: "变量",
+  4: "枚举值",
+  5: "页"
 };
 
--- static/apidoc/android/chat3.0/search/variables_2.js
+++ static/apidoc/android/chat3.0/search/variables_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['disconnected',['DISCONNECTED',['../enumcom_1_1hyphenate_1_1chat_1_1_e_m_call_state_change_listener_1_1_call_state.html#a9d270eeeddaedf1259c127d0e563f41b',1,'com::hyphenate::chat::EMCallStateChangeListener::CallState']]],
-  ['discussiongroup',['DiscussionGroup',['../enumcom_1_1hyphenate_1_1chat_1_1_e_m_conversation_1_1_e_m_conversation_type.html#ae7ed9cfbd6afc1893d8c225e2310d7bf',1,'com::hyphenate::chat::EMConversation::EMConversationType']]],
-  ['down',['DOWN',['../enumcom_1_1hyphenate_1_1chat_1_1_e_m_conversation_1_1_e_m_search_direction.html#a81600c9b26032731d06e4c41bf405a58',1,'com::hyphenate::chat::EMConversation::EMSearchDirection']]]
+  ['disconnected',['DISCONNECTED',['../enumcom_1_1hyphenate_1_1chat_1_1EMCallStateChangeListener_1_1CallState.html#a9d270eeeddaedf1259c127d0e563f41b',1,'com::hyphenate::chat::EMCallStateChangeListener::CallState']]],
+  ['discussiongroup',['DiscussionGroup',['../enumcom_1_1hyphenate_1_1chat_1_1EMConversation_1_1EMConversationType.html#ae7ed9cfbd6afc1893d8c225e2310d7bf',1,'com::hyphenate::chat::EMConversation::EMConversationType']]],
+  ['down',['DOWN',['../enumcom_1_1hyphenate_1_1chat_1_1EMConversation_1_1EMSearchDirection.html#a81600c9b26032731d06e4c41bf405a58',1,'com::hyphenate::chat::EMConversation::EMSearchDirection']]]
 ];
