319e78f750f6c7ad2bec5fbb1fa7f8519c7c6b2a
dujiepeng <347302029@qq.com>
2020-03-13 12:43:52 +0800

    update ios api to 3.6.5

--- static/apidoc/ios/chat3.0/search/properties_8.js
+++ static/apidoc/ios/chat3.0/search/properties_8.js
@@ -1,16 +1,16 @@
 var searchData=
 [
-  ['latestmessage_1298',['latestMessage',['../interface_e_m_conversation.html#a21a7bd7077a0bdffcf81fe3f282fe416',1,'EMConversation']]],
-  ['latitude_1299',['latitude',['../interface_e_m_location_message_body.html#a250f22dfc88010c5243ba0f2f389d0e2',1,'EMLocationMessageBody']]],
-  ['list_1300',['list',['../interface_e_m_cursor_result.html#abf5113042bfaf11657f1fe8ad7120cb9',1,'EMCursorResult::list()'],['../interface_e_m_page_result.html#a98428683dcf514814854c995c975940e',1,'EMPageResult::list()']]],
-  ['localname_1301',['localName',['../interface_e_m_call_conference.html#adb685e451ad124c379a18af05cd0eba8',1,'EMCallConference::localName()'],['../interface_e_m_call_session.html#a97e3bea964a2228e08cbc77587f5402e',1,'EMCallSession::localName()']]],
-  ['localpath_1302',['localPath',['../interface_e_m_file_message_body.html#aa109e9466c919f0fe5c1c9ef37b5386d',1,'EMFileMessageBody']]],
-  ['localtime_1303',['localTime',['../interface_e_m_message.html#a04e019786fcd2c149b74bff2b9336713',1,'EMMessage']]],
-  ['localvideobitrate_1304',['localVideoBitrate',['../interface_e_m_call_session.html#aecc7d56536bfedcb924e19e0bc860109',1,'EMCallSession']]],
-  ['localvideoframerate_1305',['localVideoFrameRate',['../interface_e_m_call_session.html#a6d05d54be6d7508b959f2ef020ca0eee',1,'EMCallSession']]],
-  ['localvideolostrateinpercent_1306',['localVideoLostRateInPercent',['../interface_e_m_call_session.html#ab42eb2571e0bb1111ab33f84c80588a0',1,'EMCallSession']]],
-  ['localvideoview_1307',['localVideoView',['../interface_e_m_call_session.html#ad8ed75a4fedb8ae9573d8c5ce296867a',1,'EMCallSession']]],
-  ['localview_1308',['localView',['../interface_e_m_stream_param.html#ae07d3fea0a3617f718782704d02820b3',1,'EMStreamParam']]],
-  ['loglevel_1309',['logLevel',['../interface_e_m_options.html#a0532bf2756002f639f403bed4c8a589e',1,'EMOptions']]],
-  ['longitude_1310',['longitude',['../interface_e_m_location_message_body.html#a803a3f6cd011398d9f3125092359ce67',1,'EMLocationMessageBody']]]
+  ['latestmessage',['latestMessage',['../interface_e_m_conversation.html#a21a7bd7077a0bdffcf81fe3f282fe416',1,'EMConversation']]],
+  ['latitude',['latitude',['../interface_e_m_location_message_body.html#a250f22dfc88010c5243ba0f2f389d0e2',1,'EMLocationMessageBody']]],
+  ['list',['list',['../interface_e_m_cursor_result.html#abf5113042bfaf11657f1fe8ad7120cb9',1,'EMCursorResult::list()'],['../interface_e_m_page_result.html#a98428683dcf514814854c995c975940e',1,'EMPageResult::list()']]],
+  ['localname',['localName',['../interface_e_m_call_conference.html#adb685e451ad124c379a18af05cd0eba8',1,'EMCallConference::localName()'],['../interface_e_m_call_session.html#a97e3bea964a2228e08cbc77587f5402e',1,'EMCallSession::localName()']]],
+  ['localpath',['localPath',['../interface_e_m_file_message_body.html#aa109e9466c919f0fe5c1c9ef37b5386d',1,'EMFileMessageBody']]],
+  ['localtime',['localTime',['../interface_e_m_message.html#a04e019786fcd2c149b74bff2b9336713',1,'EMMessage']]],
+  ['localvideobitrate',['localVideoBitrate',['../interface_e_m_call_session.html#aecc7d56536bfedcb924e19e0bc860109',1,'EMCallSession']]],
+  ['localvideoframerate',['localVideoFrameRate',['../interface_e_m_call_session.html#a6d05d54be6d7508b959f2ef020ca0eee',1,'EMCallSession']]],
+  ['localvideolostrateinpercent',['localVideoLostRateInPercent',['../interface_e_m_call_session.html#ab42eb2571e0bb1111ab33f84c80588a0',1,'EMCallSession']]],
+  ['localvideoview',['localVideoView',['../interface_e_m_call_session.html#ad8ed75a4fedb8ae9573d8c5ce296867a',1,'EMCallSession']]],
+  ['localview',['localView',['../interface_e_m_stream_param.html#ae07d3fea0a3617f718782704d02820b3',1,'EMStreamParam']]],
+  ['loglevel',['logLevel',['../interface_e_m_options.html#a0532bf2756002f639f403bed4c8a589e',1,'EMOptions']]],
+  ['longitude',['longitude',['../interface_e_m_location_message_body.html#a803a3f6cd011398d9f3125092359ce67',1,'EMLocationMessageBody']]]
 ];
