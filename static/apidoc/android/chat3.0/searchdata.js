b4946bd1ab4968a0067b10b421b1f8d7719c6359
zhaoliang <zhaoliang@easemob.com>
2020-04-09 11:06:49 +0800

    update api to 3.6.6

---
+++ static/apidoc/android/chat3.0/searchdata.js
@@ -0,0 +1,30 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefgijklmnoprstuvå",
+  1: "acdelmst",
+  2: "abcdefgijklmoprstu",
+  3: "acdefgimnprsuv",
+  4: "v",
+  5: "å"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions",
+  3: "variables",
+  4: "enumvalues",
+  5: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "全部",
+  1: "类",
+  2: "函数",
+  3: "变量",
+  4: "枚举值",
+  5: "页"
+};
+
