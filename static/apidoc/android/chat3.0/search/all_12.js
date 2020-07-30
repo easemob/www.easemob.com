e067a69cb36940c9a8adde53d4ceed69b685e174
lijian <727402046@qq.com>
2020-07-30 17:04:45 +0800

    modify sdk 3.7.0

--- static/apidoc/android/chat3.0/search/all_12.js
+++ static/apidoc/android/chat3.0/search/all_12.js
@@ -4,6 +4,6 @@ var searchData=
   ['thumbnaillocalpath',['thumbnailLocalPath',['../classcom_1_1hyphenate_1_1chat_1_1EMImageMessageBody.html#a7e3045c1c19573069b2b5d6e8218ea64',1,'com::hyphenate::chat::EMImageMessageBody']]],
   ['thumbnaillocaluri',['thumbnailLocalUri',['../classcom_1_1hyphenate_1_1chat_1_1EMImageMessageBody.html#acfe13064181f0b50128ed12a308ebcd4',1,'com::hyphenate::chat::EMImageMessageBody']]],
   ['tostring',['toString',['../classcom_1_1hyphenate_1_1chat_1_1EMGroup.html#a4b78922949af0c458ab80ca4c2979546',1,'com::hyphenate::chat::EMGroup']]],
-  ['type',['Type',['../enumcom_1_1hyphenate_1_1chat_1_1EMMessage_1_1Type.html',1,'com::hyphenate::chat::EMMessage']]],
-  ['type',['Type',['../enumcom_1_1hyphenate_1_1chat_1_1EMCallSession_1_1Type.html',1,'com::hyphenate::chat::EMCallSession']]]
+  ['type',['Type',['../enumcom_1_1hyphenate_1_1chat_1_1EMCallSession_1_1Type.html',1,'com::hyphenate::chat::EMCallSession']]],
+  ['type',['Type',['../enumcom_1_1hyphenate_1_1chat_1_1EMMessage_1_1Type.html',1,'com::hyphenate::chat::EMMessage']]]
 ];
