4a7b0fff570c895a883d5aa2a29ed6c5afdd0481
dujiepeng <347302029@qq.com>
2021-01-04 20:59:49 +0800

    upgrade ios doc to 3.7.3

--- static/apidoc/ios/chat3.0/search/all_8.js
+++ static/apidoc/ios/chat3.0/search/all_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['h_437',['h',['../interface_live_region.html#a70c3f1167db1e92d068e1f4d5aedc8f1',1,'LiveRegion']]],
-  ['height_438',['height',['../interface_c_d_n_canvas.html#a84752e04848cc8d74b3f4d2bdbc4bb0c',1,'CDNCanvas']]]
+  ['h',['h',['../interface_live_region.html#a70c3f1167db1e92d068e1f4d5aedc8f1',1,'LiveRegion']]],
+  ['height',['height',['../interface_c_d_n_canvas.html#a84752e04848cc8d74b3f4d2bdbc4bb0c',1,'CDNCanvas']]]
 ];
--- static/apidoc/ios/chat3.0/search/properties_6.js
+++ static/apidoc/ios/chat3.0/search/properties_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['groupid_1504',['groupId',['../interface_e_m_group.html#a743f373370bfa102f78bfc10b8791102',1,'EMGroup']]],
-  ['groupmanager_1505',['groupManager',['../interface_e_m_client.html#a7945b211dc3f2580543235f5009ec7d9',1,'EMClient']]],
-  ['groupname_1506',['groupName',['../interface_e_m_group.html#ac40a010114da7848c7df798d7297249e',1,'EMGroup']]]
+  ['groupid',['groupId',['../interface_e_m_group.html#a743f373370bfa102f78bfc10b8791102',1,'EMGroup']]],
+  ['groupmanager',['groupManager',['../interface_e_m_client.html#a7945b211dc3f2580543235f5009ec7d9',1,'EMClient']]],
+  ['groupname',['groupName',['../interface_e_m_group.html#ac40a010114da7848c7df798d7297249e',1,'EMGroup']]]
 ];
