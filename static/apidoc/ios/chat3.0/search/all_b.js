579722260ab33a22c876a4b4f24638aac28df949
朱继超 <984065974@qq.com>
2022-11-07 18:54:43 +0800

    3.9.8 iOS  apiDoc

--- static/apidoc/ios/chat3.0/search/all_b.js
+++ static/apidoc/ios/chat3.0/search/all_b.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['kickalldeviceswithusername_3apassword_3a_0',['kickAllDevicesWithUsername:password:',['../interface_e_m_client.html#a652408322ab1ebfb2c7dfd53fd750c0d',1,'EMClient']]],
-  ['kickalldeviceswithusername_3apassword_3acompletion_3a_1',['kickAllDevicesWithUsername:password:completion:',['../interface_e_m_client.html#a5971778fb7d33d1229d366d63d604605',1,'EMClient']]],
-  ['kickdevicewithusername_3apassword_3aresource_3a_2',['kickDeviceWithUsername:password:resource:',['../interface_e_m_client.html#ad8d8839ed2c65bf2aef0ae0caffbb455',1,'EMClient']]],
-  ['kickdevicewithusername_3apassword_3aresource_3acompletion_3a_3',['kickDeviceWithUsername:password:resource:completion:',['../interface_e_m_client.html#a3243373372113b9fe84b89c1ff8d5456',1,'EMClient']]]
+  ['kickalldeviceswithusername_3apassword_3a',['kickAllDevicesWithUsername:password:',['../interface_e_m_client.html#a652408322ab1ebfb2c7dfd53fd750c0d',1,'EMClient']]],
+  ['kickalldeviceswithusername_3apassword_3acompletion_3a',['kickAllDevicesWithUsername:password:completion:',['../interface_e_m_client.html#a5971778fb7d33d1229d366d63d604605',1,'EMClient']]],
+  ['kickdevicewithusername_3apassword_3aresource_3a',['kickDeviceWithUsername:password:resource:',['../interface_e_m_client.html#ad8d8839ed2c65bf2aef0ae0caffbb455',1,'EMClient']]],
+  ['kickdevicewithusername_3apassword_3aresource_3acompletion_3a',['kickDeviceWithUsername:password:resource:completion:',['../interface_e_m_client.html#a3243373372113b9fe84b89c1ff8d5456',1,'EMClient']]]
 ];
--- static/apidoc/ios/chat3.0/search/properties_0.js
+++ static/apidoc/ios/chat3.0/search/properties_0.js
@@ -1,15 +1,15 @@
 var searchData=
 [
-  ['accessusertoken_0',['accessUserToken',['../interface_e_m_client.html#a2fa34ffc2d955950651534a97cf269e3',1,'EMClient']]],
-  ['action_1',['action',['../interface_e_m_cmd_message_body.html#a52133aa1e7e36dc3699550c56752ba86',1,'EMCmdMessageBody']]],
-  ['address_2',['address',['../interface_e_m_location_message_body.html#a3a8cb0a38dea43be09300702053997f4',1,'EMLocationMessageBody']]],
-  ['adminlist_3',['adminList',['../interface_e_m_chatroom.html#a2a90fa560817d96482458e68c09b883a',1,'EMChatroom::adminList()'],['../interface_e_m_group.html#ab79a83b7f7339240ab3342f69e62e245',1,'EMGroup::adminList()']]],
-  ['announcement_4',['announcement',['../interface_e_m_chatroom.html#ab82c203fa0342ba69ea61caa74fd1f3a',1,'EMChatroom::announcement()'],['../interface_e_m_group.html#a1ae69e0c6fdc8a63a9e0fbafa0779805',1,'EMGroup::announcement()']]],
-  ['apnscertname_5',['apnsCertName',['../interface_e_m_options.html#a0bc4133a846b0c769ec6f47277473dc5',1,'EMOptions']]],
-  ['appkey_6',['appkey',['../interface_e_m_options.html#a3a691b8da310dbbeaffd87f1e00c56eb',1,'EMOptions']]],
-  ['area_7',['area',['../interface_e_m_options.html#a9f0d7242cdcee3ab31ca95c9bcbd346f',1,'EMOptions']]],
-  ['autoacceptfriendinvitation_8',['autoAcceptFriendInvitation',['../interface_e_m_options.html#a813f374c2a7a91817e28e4296f81f199',1,'EMOptions']]],
-  ['autoacceptgroupinvitation_9',['autoAcceptGroupInvitation',['../interface_e_m_options.html#a627c9f7933d3e78fa168cdff82119897',1,'EMOptions']]],
-  ['autodownloadthumbnail_10',['autoDownloadThumbnail',['../interface_e_m_options.html#a23c293667dd658b23b5497c80243c198',1,'EMOptions']]],
-  ['avatarurl_11',['avatarUrl',['../interface_e_m_user_info.html#ae21848d71c809557e22ffc59121e6dd1',1,'EMUserInfo']]]
+  ['accessusertoken',['accessUserToken',['../interface_e_m_client.html#a2fa34ffc2d955950651534a97cf269e3',1,'EMClient']]],
+  ['action',['action',['../interface_e_m_cmd_message_body.html#a52133aa1e7e36dc3699550c56752ba86',1,'EMCmdMessageBody']]],
+  ['address',['address',['../interface_e_m_location_message_body.html#a3a8cb0a38dea43be09300702053997f4',1,'EMLocationMessageBody']]],
+  ['adminlist',['adminList',['../interface_e_m_chatroom.html#a2a90fa560817d96482458e68c09b883a',1,'EMChatroom::adminList()'],['../interface_e_m_group.html#ab79a83b7f7339240ab3342f69e62e245',1,'EMGroup::adminList()']]],
+  ['announcement',['announcement',['../interface_e_m_chatroom.html#ab82c203fa0342ba69ea61caa74fd1f3a',1,'EMChatroom::announcement()'],['../interface_e_m_group.html#a1ae69e0c6fdc8a63a9e0fbafa0779805',1,'EMGroup::announcement()']]],
+  ['apnscertname',['apnsCertName',['../interface_e_m_options.html#a0bc4133a846b0c769ec6f47277473dc5',1,'EMOptions']]],
+  ['appkey',['appkey',['../interface_e_m_options.html#a3a691b8da310dbbeaffd87f1e00c56eb',1,'EMOptions']]],
+  ['area',['area',['../interface_e_m_options.html#a9f0d7242cdcee3ab31ca95c9bcbd346f',1,'EMOptions']]],
+  ['autoacceptfriendinvitation',['autoAcceptFriendInvitation',['../interface_e_m_options.html#a813f374c2a7a91817e28e4296f81f199',1,'EMOptions']]],
+  ['autoacceptgroupinvitation',['autoAcceptGroupInvitation',['../interface_e_m_options.html#a627c9f7933d3e78fa168cdff82119897',1,'EMOptions']]],
+  ['autodownloadthumbnail',['autoDownloadThumbnail',['../interface_e_m_options.html#a23c293667dd658b23b5497c80243c198',1,'EMOptions']]],
+  ['avatarurl',['avatarUrl',['../interface_e_m_user_info.html#ae21848d71c809557e22ffc59121e6dd1',1,'EMUserInfo']]]
 ];
