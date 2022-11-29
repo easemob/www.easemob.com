31e6473a0071fccc18cc7ebf42e79f371168d010
lixm1988 <527725681@qq.com>
2022-11-29 10:41:34 +0800

    update iOS 3.9.9 api doc

--- static/apidoc/ios/chat3.0/search/functions_11.js
+++ static/apidoc/ios/chat3.0/search/functions_11.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['tokendidexpire_3a',['tokenDidExpire:',['../protocol_e_m_client_delegate-p.html#a8815169ea17fa723db3903e307064540',1,'EMClientDelegate-p']]],
-  ['tokenwillexpire_3a',['tokenWillExpire:',['../protocol_e_m_client_delegate-p.html#aa4502cad30fd6d4d467d1eff375df853',1,'EMClientDelegate-p']]],
-  ['translatemessage_3atargetlanguages_3acompletion_3a',['translateMessage:targetLanguages:completion:',['../protocol_i_e_m_chat_manager-p.html#a45ed7c2137614ee7fed6922d40cf828c',1,'IEMChatManager-p']]]
+  ['tokendidexpire_3a_0',['tokenDidExpire:',['../protocol_e_m_client_delegate-p.html#a8815169ea17fa723db3903e307064540',1,'EMClientDelegate-p']]],
+  ['tokenwillexpire_3a_1',['tokenWillExpire:',['../protocol_e_m_client_delegate-p.html#aa4502cad30fd6d4d467d1eff375df853',1,'EMClientDelegate-p']]],
+  ['translatemessage_3atargetlanguages_3acompletion_3a_2',['translateMessage:targetLanguages:completion:',['../protocol_i_e_m_chat_manager-p.html#a23eef0b7f10c3fc7bd513030e2019664',1,'IEMChatManager-p']]]
 ];
