f064f486c1127db8f1e34645f47e734e5569ffdc
lixm1988 <527725681@qq.com>
2023-04-25 15:28:32 +0800

    update iOS SDK 4.0.2 apiDoc

--- static/apidoc/ios/chat3.0/search/properties_a.js
+++ static/apidoc/ios/chat3.0/search/properties_a.js
@@ -1,16 +1,17 @@
 var searchData=
 [
-  ['mail',['mail',['../interface_e_m_user_info.html#a7772a7f2392cdf184cfad3b537985c17',1,'EMUserInfo']]],
-  ['maxoccupantscount',['maxOccupantsCount',['../interface_e_m_chatroom.html#a1be7d7507769472b119c8c0c09bb2ba3',1,'EMChatroom']]],
-  ['maxusers',['maxUsers',['../interface_e_m_group_options.html#ac1c4abb031008423d8e60493d85cd9e2',1,'EMGroupOptions']]],
-  ['maxuserscount',['maxUsersCount',['../interface_e_m_chatroom_options.html#a0440ab078134a82d3b503959f72bbc36',1,'EMChatroomOptions']]],
-  ['memberlist',['memberList',['../interface_e_m_chatroom.html#a9af689060027755f38d5981b614c2766',1,'EMChatroom::memberList()'],['../interface_e_m_group.html#ab4b47644041818bc040921857777ed56',1,'EMGroup::memberList()']]],
-  ['memberscount',['membersCount',['../interface_e_m_chat_thread.html#a2cbbd0878d5bfa53fcf36be3576c688c',1,'EMChatThread']]],
-  ['messagecount',['messageCount',['../interface_e_m_chat_thread.html#aea0f4afb695dbdc5deba4488b98055e7',1,'EMChatThread']]],
-  ['messageid',['messageId',['../interface_e_m_chat_message.html#a87118c17f667d7cfdf8c0f61e09dd3cf',1,'EMChatMessage::messageId()'],['../interface_e_m_chat_message_statistics.html#a810915cb9c2cb1f28f4b2a52733171c3',1,'EMChatMessageStatistics::messageId()'],['../interface_e_m_chat_thread.html#abc9c83516b0b544d97a2c67ec17288ee',1,'EMChatThread::messageId()'],['../interface_e_m_group_message_ack.html#ae1ef2fc01bf14c2b907c6015a31320cf',1,'EMGroupMessageAck::messageId()'],['../interface_e_m_message_reaction_change.html#a94c941e311102d5172ae4a11fb75e8d1',1,'EMMessageReactionChange::messageId()']]],
-  ['messagescount',['messagesCount',['../interface_e_m_conversation.html#a49ec21b882b40ae12e868978a8ec688d',1,'EMConversation']]],
-  ['messagesize',['messageSize',['../interface_e_m_chat_message_statistics.html#ada7c0074b4a4cf945c1bd41ab048b0c1',1,'EMChatMessageStatistics']]],
-  ['minutes',['minutes',['../interface_e_m_silent_mode_time.html#a29ec3446b6d0329dc607bdcf064aa58a',1,'EMSilentModeTime']]],
-  ['msgid',['msgId',['../interface_e_m_translation_result.html#a37cc8b8acf886e9990b9416ca5f164d8',1,'EMTranslationResult']]],
-  ['mutelist',['muteList',['../interface_e_m_chatroom.html#a2ec9b6e10f7299b1414f4758f271e454',1,'EMChatroom::muteList()'],['../interface_e_m_group.html#a54ff97e40a487cf2af74beee9e78386d',1,'EMGroup::muteList()']]]
+  ['mail_0',['mail',['../interface_e_m_user_info.html#a7772a7f2392cdf184cfad3b537985c17',1,'EMUserInfo']]],
+  ['maxoccupantscount_1',['maxOccupantsCount',['../interface_e_m_chatroom.html#a1be7d7507769472b119c8c0c09bb2ba3',1,'EMChatroom']]],
+  ['maxusers_2',['maxUsers',['../interface_e_m_group_options.html#ac1c4abb031008423d8e60493d85cd9e2',1,'EMGroupOptions']]],
+  ['maxuserscount_3',['maxUsersCount',['../interface_e_m_chatroom_options.html#a0440ab078134a82d3b503959f72bbc36',1,'EMChatroomOptions']]],
+  ['memberlist_4',['memberList',['../interface_e_m_chatroom.html#a9af689060027755f38d5981b614c2766',1,'EMChatroom::memberList()'],['../interface_e_m_group.html#ab4b47644041818bc040921857777ed56',1,'EMGroup::memberList()']]],
+  ['memberscount_5',['membersCount',['../interface_e_m_chat_thread.html#a2cbbd0878d5bfa53fcf36be3576c688c',1,'EMChatThread']]],
+  ['messagecount_6',['messageCount',['../interface_e_m_chat_thread.html#aea0f4afb695dbdc5deba4488b98055e7',1,'EMChatThread']]],
+  ['messageid_7',['messageId',['../interface_e_m_chat_message.html#a87118c17f667d7cfdf8c0f61e09dd3cf',1,'EMChatMessage::messageId()'],['../interface_e_m_chat_message_statistics.html#a810915cb9c2cb1f28f4b2a52733171c3',1,'EMChatMessageStatistics::messageId()'],['../interface_e_m_chat_thread.html#abc9c83516b0b544d97a2c67ec17288ee',1,'EMChatThread::messageId()'],['../interface_e_m_group_message_ack.html#ae1ef2fc01bf14c2b907c6015a31320cf',1,'EMGroupMessageAck::messageId()'],['../interface_e_m_message_reaction_change.html#a94c941e311102d5172ae4a11fb75e8d1',1,'EMMessageReactionChange::messageId()']]],
+  ['messagescount_8',['messagesCount',['../interface_e_m_conversation.html#a49ec21b882b40ae12e868978a8ec688d',1,'EMConversation']]],
+  ['messagesize_9',['messageSize',['../interface_e_m_chat_message_statistics.html#ada7c0074b4a4cf945c1bd41ab048b0c1',1,'EMChatMessageStatistics']]],
+  ['minutes_10',['minutes',['../interface_e_m_silent_mode_time.html#a29ec3446b6d0329dc607bdcf064aa58a',1,'EMSilentModeTime']]],
+  ['msgid_11',['msgId',['../interface_e_m_translation_result.html#a37cc8b8acf886e9990b9416ca5f164d8',1,'EMTranslationResult']]],
+  ['msgtypes_12',['msgTypes',['../interface_e_m_fetch_server_messages_option.html#a87ae35089eaf61188206c7029930d527',1,'EMFetchServerMessagesOption']]],
+  ['mutelist_13',['muteList',['../interface_e_m_chatroom.html#a2ec9b6e10f7299b1414f4758f271e454',1,'EMChatroom::muteList()'],['../interface_e_m_group.html#a54ff97e40a487cf2af74beee9e78386d',1,'EMGroup::muteList()']]]
 ];
--- static/apidoc/ios/chat3.0/search/properties_b.js
+++ static/apidoc/ios/chat3.0/search/properties_b.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['nickname',['nickname',['../interface_e_m_user_info.html#aea54951c9e03e57a361cfb8e010c462e',1,'EMUserInfo']]]
+  ['nickname_0',['nickname',['../interface_e_m_user_info.html#aea54951c9e03e57a361cfb8e010c462e',1,'EMUserInfo']]]
 ];
