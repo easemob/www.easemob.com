105692d0663d5e74af873f2b458c601c3c37ff9d
lixm1988 <527725681@qq.com>
2023-06-15 14:03:39 +0800

    udpate iOS sdk apiDoc to 4.0.3

--- static/apidoc/ios/chat3.0/search/functions_e.js
+++ static/apidoc/ios/chat3.0/search/functions_e.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['pageresultwithlist_3aandcount_3a_0',['pageResultWithList:andCount:',['../interface_e_m_page_result.html#a34c677afbb3c3eb68b1b1c924ccd6fe0',1,'EMPageResult']]],
-  ['presencestatusdidchanged_3a_1',['presenceStatusDidChanged:',['../protocol_e_m_presence_manager_delegate-p.html#a16c1f61f6a0fa67c5402401b4535bf25',1,'EMPresenceManagerDelegate-p']]],
-  ['publishpresencewithdescription_3acompletion_3a_2',['publishPresenceWithDescription:completion:',['../protocol_i_e_m_presence_manager-p.html#a8e27652e793406682aba73d269c53108',1,'IEMPresenceManager-p']]]
+  ['pinconversation_3aispinned_3acompletionblock_3a_1',['pinConversation:isPinned:completionBlock:',['../protocol_i_e_m_chat_manager-p.html#ab7ec272edae5130035bd09f6edc88c24',1,'IEMChatManager-p']]],
+  ['presencestatusdidchanged_3a_2',['presenceStatusDidChanged:',['../protocol_e_m_presence_manager_delegate-p.html#a16c1f61f6a0fa67c5402401b4535bf25',1,'EMPresenceManagerDelegate-p']]],
+  ['publishpresencewithdescription_3acompletion_3a_3',['publishPresenceWithDescription:completion:',['../protocol_i_e_m_presence_manager-p.html#a8e27652e793406682aba73d269c53108',1,'IEMPresenceManager-p']]]
 ];
