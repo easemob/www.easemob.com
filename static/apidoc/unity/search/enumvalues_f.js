14941b3a0d01e29fea316fc320a29b2047c6de65
luckyyuqiang <luckyyuqiang@163.com>
2023-12-25 11:21:35 +0800

    Recommit API reference for Unity/Windows.

--- static/apidoc/unity/search/enumvalues_f.js
+++ static/apidoc/unity/search/enumvalues_f.js
@@ -1,12 +1,7 @@
 var searchData=
 [
   ['send_0',['SEND',['../namespace_agora_chat.html#ac17636c7a7b633f9c185876d850a44eea548e51fa67d541384e9585adf0db95dc',1,'AgoraChat']]],
-<<<<<<< HEAD
   ['set_5fmetadata_1',['SET_METADATA',['../namespace_agora_chat.html#a219e2b8202944dcdef170dc529f8fe68a9bda5c8e0c075527c2c0d06abf19ec1b',1,'AgoraChat']]],
   ['string_2',['STRING',['../namespace_agora_chat.html#a292bc538f956ba5bf2976941df4b469ba63b588d5559f64f89a416e656880b949',1,'AgoraChat']]],
   ['success_3',['SUCCESS',['../namespace_agora_chat.html#a33be8ac1aa48dbbed69d5a7f40ffb131ad0749aaba8b833466dfcbb0428e4f89c',1,'AgoraChat.SUCCESS()'],['../namespace_agora_chat_1_1_message_body.html#a1c68f57689f3139b999693c8fec4546aad0749aaba8b833466dfcbb0428e4f89c',1,'AgoraChat.MessageBody.SUCCESS()']]]
-=======
-  ['string_1',['STRING',['../namespace_agora_chat.html#a292bc538f956ba5bf2976941df4b469ba63b588d5559f64f89a416e656880b949',1,'AgoraChat']]],
-  ['success_2',['SUCCESS',['../namespace_agora_chat.html#a33be8ac1aa48dbbed69d5a7f40ffb131ad0749aaba8b833466dfcbb0428e4f89c',1,'AgoraChat.SUCCESS()'],['../namespace_agora_chat_1_1_message_body.html#a1c68f57689f3139b999693c8fec4546aad0749aaba8b833466dfcbb0428e4f89c',1,'AgoraChat.MessageBody.SUCCESS()']]]
->>>>>>> upstream/master
 ];
--- static/apidoc/unity/search/functions_e.js
+++ static/apidoc/unity/search/functions_e.js
@@ -6,12 +6,7 @@ var searchData=
   ['sendmessagereadack_3',['SendMessageReadAck',['../class_agora_chat_1_1_chat_manager.html#af9cca4dff53384c723789d3fda294238',1,'AgoraChat::ChatManager']]],
   ['sendreadackforgroupmessage_4',['SendReadAckForGroupMessage',['../class_agora_chat_1_1_chat_manager.html#a4da90d42232684a08d5e95d8251812b3',1,'AgoraChat::ChatManager']]],
   ['setattribute_5',['SetAttribute',['../class_agora_chat_1_1_message.html#a68bf990a3120b2209e34f1c022993251',1,'AgoraChat::Message']]],
-<<<<<<< HEAD
   ['setmemberattributes_6',['SetMemberAttributes',['../class_agora_chat_1_1_group_manager.html#ad10020882ad1f6ed528f9c29703f7a20',1,'AgoraChat::GroupManager']]],
   ['setroommessagepriority_7',['SetRoomMessagePriority',['../class_agora_chat_1_1_message.html#ade07d43948f84110075013bb67df4cc1',1,'AgoraChat::Message']]],
   ['subscribepresences_8',['SubscribePresences',['../class_agora_chat_1_1_presence_manager.html#a45d352f595e5f8fcabfaa1a8650cc5a0',1,'AgoraChat::PresenceManager']]]
-=======
-  ['setroommessagepriority_6',['SetRoomMessagePriority',['../class_agora_chat_1_1_message.html#ade07d43948f84110075013bb67df4cc1',1,'AgoraChat::Message']]],
-  ['subscribepresences_7',['SubscribePresences',['../class_agora_chat_1_1_presence_manager.html#a45d352f595e5f8fcabfaa1a8650cc5a0',1,'AgoraChat::PresenceManager']]]
->>>>>>> upstream/master
 ];
