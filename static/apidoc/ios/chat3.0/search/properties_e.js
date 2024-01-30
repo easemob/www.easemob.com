2cab19ccd1cfb5dc252e1d0a413bd78f2115adef
lixm1988 <527725681@qq.com>
2024-01-30 15:50:02 +0800

    update iOS SDK 4.4.0 apiDoc

--- static/apidoc/ios/chat3.0/search/properties_e.js
+++ static/apidoc/ios/chat3.0/search/properties_e.js
@@ -8,10 +8,11 @@ var searchData=
   ['recallby_5',['recallBy',['../interface_e_m_recall_message_info.html#a2d7cdb67a0f5e5b874738fbcb981dc75',1,'EMRecallMessageInfo']]],
   ['recallmessage_6',['recallMessage',['../interface_e_m_recall_message_info.html#a073868eadb736c11af52ba07ff4c4787',1,'EMRecallMessageInfo']]],
   ['receiverlist_7',['receiverList',['../interface_e_m_chat_message.html#afcdb8777948282281bdbd23ba4d1fcea',1,'EMChatMessage']]],
-  ['remark_8',['remark',['../interface_e_m_contact.html#a534e30116a18a324399ffa315b4b5ad1',1,'EMContact']]],
-  ['remindtype_9',['remindType',['../interface_e_m_silent_mode_param.html#a5ce628156cd3d11bd25af24473226533',1,'EMSilentModeParam::remindType()'],['../interface_e_m_silent_mode_result.html#a36c9c4dcc6bd00b331bedad657cedb14',1,'EMSilentModeResult::remindType()']]],
-  ['remotepath_10',['remotePath',['../interface_e_m_file_message_body.html#a26be76c47f8ce7e0cb2f24b2e95b10d3',1,'EMFileMessageBody']]],
-  ['resource_11',['resource',['../interface_e_m_device_config.html#ad7b34d60d92b577bb36671ce9a92c367',1,'EMDeviceConfig']]],
-  ['restserver_12',['restServer',['../category_e_m_options_07_private_deploy_08.html#a534277c754dd1448227b01fa2401d68d',1,'EMOptions(PrivateDeploy)::restServer()'],['../interface_e_m_options.html#a534277c754dd1448227b01fa2401d68d',1,'EMOptions::restServer()']]],
-  ['roommanager_13',['roomManager',['../interface_e_m_client.html#a4452e4b2d78d5ed52bc202b46dfdb1c5',1,'EMClient']]]
+  ['regardimportmessagesasread_8',['regardImportMessagesAsRead',['../interface_e_m_options.html#a06241d12a9c2fb48aff520d2ef03008a',1,'EMOptions']]],
+  ['remark_9',['remark',['../interface_e_m_contact.html#a534e30116a18a324399ffa315b4b5ad1',1,'EMContact']]],
+  ['remindtype_10',['remindType',['../interface_e_m_silent_mode_param.html#a5ce628156cd3d11bd25af24473226533',1,'EMSilentModeParam::remindType()'],['../interface_e_m_silent_mode_result.html#a36c9c4dcc6bd00b331bedad657cedb14',1,'EMSilentModeResult::remindType()']]],
+  ['remotepath_11',['remotePath',['../interface_e_m_file_message_body.html#a26be76c47f8ce7e0cb2f24b2e95b10d3',1,'EMFileMessageBody']]],
+  ['resource_12',['resource',['../interface_e_m_device_config.html#ad7b34d60d92b577bb36671ce9a92c367',1,'EMDeviceConfig']]],
+  ['restserver_13',['restServer',['../category_e_m_options_07_private_deploy_08.html#a534277c754dd1448227b01fa2401d68d',1,'EMOptions(PrivateDeploy)::restServer()'],['../interface_e_m_options.html#a534277c754dd1448227b01fa2401d68d',1,'EMOptions::restServer()']]],
+  ['roommanager_14',['roomManager',['../interface_e_m_client.html#a4452e4b2d78d5ed52bc202b46dfdb1c5',1,'EMClient']]]
 ];
