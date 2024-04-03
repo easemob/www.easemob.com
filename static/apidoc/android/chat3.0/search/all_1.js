26a26f89fc6b199450a13bf70c6073c177a41df3
xuchengpu mac <1550540124@qq.com>
2024-04-03 14:11:50 +0800

    update android doc version to 4.5.0

--- static/apidoc/android/chat3.0/search/all_1.js
+++ static/apidoc/android/chat3.0/search/all_1.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['be_5fkicked_0',['BE_KICKED',['../interfacecom_1_1hyphenate_1_1_e_m_chat_room_change_listener.html#a90273c80a851076356cb05ae28bc15b5',1,'com::hyphenate::EMChatRoomChangeListener']]],
   ['be_5fkicked_5ffor_5foffline_1',['BE_KICKED_FOR_OFFLINE',['../interfacecom_1_1hyphenate_1_1_e_m_chat_room_change_listener.html#a1e5190b085fce273d40d8e47dc9851af',1,'com::hyphenate::EMChatRoomChangeListener']]],
-  ['binddevicetoken_2',['bindDeviceToken',['../classcom_1_1hyphenate_1_1chat_1_1_e_m_push_manager.html#a461301f3f9dafc148cbbac204939aa48',1,'com::hyphenate::chat::EMPushManager']]],
+  ['binddevicetoken_2',['bindDeviceToken',['../classcom_1_1hyphenate_1_1chat_1_1_e_m_push_manager.html#a3c49e8245c25954b2cc1a13e93b57e0f',1,'com::hyphenate::chat::EMPushManager']]],
   ['blockchatroommembers_3',['blockChatroomMembers',['../classcom_1_1hyphenate_1_1chat_1_1_e_m_chat_room_manager.html#a3702fc4ccc4365b265fa97582caca695',1,'com::hyphenate::chat::EMChatRoomManager']]],
   ['blockgroupmessage_4',['blockGroupMessage',['../classcom_1_1hyphenate_1_1chat_1_1_e_m_group_manager.html#a7a43485f1dc97d376ca5a748e49ed4f0',1,'com::hyphenate::chat::EMGroupManager']]],
   ['blockuser_5',['blockUser',['../classcom_1_1hyphenate_1_1chat_1_1_e_m_group_manager.html#ac56c6e9f48ab00b665480fdf94b65804',1,'com::hyphenate::chat::EMGroupManager']]],
