a9461e24f9eeb272c89148e3a4f5c47f8c3a72e4
lixiaoming2 <527725681@qq.com>
2026-03-20 10:29:00 +0800

    update ios apidoc 4.20.0

--- static/apidoc/ios/chat3.0/search/functions_13.js
+++ static/apidoc/ios/chat3.0/search/functions_13.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['version',['version',['../interface_e_m_client.html#acff90f1655e88f4acbad4b82dc93147e',1,'EMClient']]]
+  ['version_0',['version',['../interface_e_m_client.html#acff90f1655e88f4acbad4b82dc93147e',1,'EMClient']]]
 ];
--- static/apidoc/ios/chat3.0/search/properties_13.js
+++ static/apidoc/ios/chat3.0/search/properties_13.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['version',['version',['../interface_e_m_client.html#a6f456f10c1dce367f322feca1bb100f8',1,'EMClient']]]
+  ['version_0',['version',['../interface_e_m_client.html#a6f456f10c1dce367f322feca1bb100f8',1,'EMClient']]]
 ];
