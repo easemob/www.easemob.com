01300b5c14fba381bc1d999b4c57ced90ce7684a
lixiaoming2 <527725681@qq.com>
2026-06-10 15:59:19 +0800

    update iOS SDK 4.23 apiDoc

--- static/apidoc/ios/chat3.0/search/functions_e.js
+++ static/apidoc/ios/chat3.0/search/functions_e.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['pageresultwithlist_3aandcount_3a',['pageResultWithList:andCount:',['../interface_e_m_page_result.html#a34c677afbb3c3eb68b1b1c924ccd6fe0',1,'EMPageResult']]],
-  ['pinconversation_3aispinned_3acompletionblock_3a',['pinConversation:isPinned:completionBlock:',['../protocol_i_e_m_chat_manager-p.html#a31782fdef367ef222c6b8890b2e660ed',1,'IEMChatManager-p']]],
-  ['pinmessage_3acompletion_3a',['pinMessage:completion:',['../protocol_i_e_m_chat_manager-p.html#a59c37d7733f37616d9d948aa5bbb7ef0',1,'IEMChatManager-p']]],
-  ['pinnedmessages',['pinnedMessages',['../interface_e_m_conversation.html#a1268b2d8c4074ce51384603908ae8ff0',1,'EMConversation']]],
-  ['presencestatusdidchanged_3a',['presenceStatusDidChanged:',['../protocol_e_m_presence_manager_delegate-p.html#a16c1f61f6a0fa67c5402401b4535bf25',1,'EMPresenceManagerDelegate-p']]],
-  ['publishpresencewithdescription_3acompletion_3a',['publishPresenceWithDescription:completion:',['../protocol_i_e_m_presence_manager-p.html#a8e27652e793406682aba73d269c53108',1,'IEMPresenceManager-p']]]
+  ['pageresultwithlist_3aandcount_3a_0',['pageResultWithList:andCount:',['../interface_e_m_page_result.html#a34c677afbb3c3eb68b1b1c924ccd6fe0',1,'EMPageResult']]],
+  ['pinconversation_3aispinned_3acompletionblock_3a_1',['pinConversation:isPinned:completionBlock:',['../protocol_i_e_m_chat_manager-p.html#ab7ec272edae5130035bd09f6edc88c24',1,'IEMChatManager-p']]],
+  ['pinmessage_3acompletion_3a_2',['pinMessage:completion:',['../protocol_i_e_m_chat_manager-p.html#a59c37d7733f37616d9d948aa5bbb7ef0',1,'IEMChatManager-p']]],
+  ['pinnedmessages_3',['pinnedMessages',['../interface_e_m_conversation.html#ab114dcaeb3dbdb46887c7735854eb1a7',1,'EMConversation']]],
+  ['presencestatusdidchanged_3a_4',['presenceStatusDidChanged:',['../protocol_e_m_presence_manager_delegate-p.html#a16c1f61f6a0fa67c5402401b4535bf25',1,'EMPresenceManagerDelegate-p']]],
+  ['publishpresencewithdescription_3acompletion_3a_5',['publishPresenceWithDescription:completion:',['../protocol_i_e_m_presence_manager-p.html#a8e27652e793406682aba73d269c53108',1,'IEMPresenceManager-p']]]
 ];
